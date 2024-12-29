1a23e6c7413a03b80cf31fe9f272affcdf10280b
donghyeonshin <donghyun4591@gmail.com>
2024-12-29 16:56:29 +0900

    feat: 로그인 버튼 클릭 시 로딩 애니메이션 추가

---
+++ frontend/app/static/js/login.js
@@ -0,0 +1,6 @@
+// ./frontend/app/static/js/login.js
+
+function showLoading() {
+  const loadingOverlay = document.getElementById('loading-overlay');
+  loadingOverlay.style.display = 'flex'; // 로딩 애니메이션 표시
+}
